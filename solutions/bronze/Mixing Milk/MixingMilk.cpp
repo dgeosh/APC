674a65c40a97026c7d8f5d6116866a99ffa58569
josh <113945456+dgeosh@users.noreply.github.com>
2023-09-15 10:06:37 -0700

    Create MixingMilk.cpp

---
+++ solutions/bronze/Mixing Milk/MixingMilk.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	freopen("mixmilk.in", "r", stdin);
+
+	vector<int> capacity(3);
+	vector<int> milk(3);
+
+	for (int i = 0; i < 3; i++) { scanf("%d %d", &capacity[i], &milk[i]); }
+	
+	for (int i = 0; i < 100; i++) {
+		int bucket1 = i % 3;
+		int bucket2 = (i + 1) % 3;
+
+		int amt = min(milk[bucket1], capacity[bucket2] - milk[bucket2]);
+
+		milk[bucket1] -= amt;
+		milk[bucket2] += amt;
+	}
+
+	freopen("mixmilk.out", "w", stdout);
+	for (int m : milk) { printf("%d\n", m); }
+}
